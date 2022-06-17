e951da51c1c144fc862aad55e0e6ab91a2172109
Hojat <fathpour.hojat@gmail.com>
2022-06-17 05:37:36 +0430

    copy and paste client folder

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,30 +1,34 @@
-const API_URL = "v1"
+const API_URL = 'v1';
+
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  const reaponse = await fetch(`${API_URL}/planets`)
-  return await reaponse.json() // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  const reaponse = await fetch(`${API_URL}/launches`)
-  const fetchedLaunches = await reaponse.json()
-  return await fetchedLaunches.sort((a, b) => {
-    return a.flightNumber - b.flightNumber
-  })
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
-      method: 'POST',
+      method: "post",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(launch)
-    })
-  } catch (err) {
-    return { ok: false }
+      body: JSON.stringify(launch),
+    });
+  } catch(err) {
+    return {
+      ok: false,
+    };
   }
 }
 
@@ -32,11 +36,13 @@ async function httpSubmitLaunch(launch) {
 async function httpAbortLaunch(id) {
   try {
     return await fetch(`${API_URL}/launches/${id}`, {
-      method: 'DELETE'
-    })
-  } catch (err) {
-    console.log(err)
-    return { ok: false }
+      method: "delete",
+    });
+  } catch(err) {
+    console.log(err);
+    return {
+      ok: false,
+    };
   }
 }
 
